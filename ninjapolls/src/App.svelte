1b4e925adedf9e32dfe11ee505879e6ddcf5f4b6
alex <alex.iconia.tab@gmail.com>
2022-03-10 20:29:00 +0200

    Add header and footer

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,12 +1,16 @@
 <script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
-<main>
-  <p>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed tenetur aspernatur, itaque soluta exercitationem alias
-    repellendus iusto velit mollitia eligendi, dignissimos omnis non? Eius sit ea nihil debitis molestias dolore.
-  </p>
-</main>
+<Header />
+<main></main>
+<Footer />
 
 <style>
+  main {
+    width: 100%;
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
