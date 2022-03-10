8a6205074e3b0cac76d40731e52127dbca80b5aa
alex <alex.iconia.tab@gmail.com>
2022-03-10 20:52:05 +0200

    Add tabs component

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,10 +1,26 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  // tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  };
 </script>
 
 <Header />
-<main></main>
+<main>
+  <Tabs items="{items}" activeItem="{activeItem}" on:tabChange="{tabChange}" />
+  {#if activeItem === "Current Polls"}
+    <p>Poll list component goes here</p>
+  {:else if activeItem === "Add New Poll"}
+    <p>New poll form component goes here</p>
+  {/if}
+</main>
 <Footer />
 
 <style>
