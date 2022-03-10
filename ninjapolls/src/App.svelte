700c900bda60931ccd914603144ba408f82d2916
alex <alex.iconia.tab@gmail.com>
2022-03-10 18:08:57 +0200

    Formatting

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,11 +1,12 @@
-<script>
+<style>
+</style>
 
+<script>
 </script>
 
 <main>
-	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed tenetur aspernatur, itaque soluta exercitationem alias repellendus iusto velit mollitia eligendi, dignissimos omnis non? Eius sit ea nihil debitis molestias dolore.</p>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed tenetur aspernatur, itaque soluta exercitationem alias
+    repellendus iusto velit mollitia eligendi, dignissimos omnis non? Eius sit ea nihil debitis molestias dolore.
+  </p>
 </main>
-
-<style>
-
-</style>
